70786971a88c5fc48fab5a818102e74c691c2e80
Cory Dodt <cdodt@aerospike.com>
2022-05-13 22:02:59 +0000

    fix lowercase note

--- src/bin/notes.rs
+++ src/bin/notes.rs
@@ -5,5 +5,5 @@ use abcrab::pitch;
 fn main() {
     let aa: Vec<String> = env::args().collect();
     let (_, note) = pitch(aa[1].as_str()).unwrap();
-    println!("{:?} {}", note, note);
+    println!("{}", note);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -82,7 +82,7 @@ impl Display for Note {
         if let Some(a) = &self.accidental {
             a.fmt(f)?
         };
-        write!(f, " {}", NOTES[(5 + self.length) as usize])?;
+        write!(f, " {}", NOTES[(3 + self.length) as usize])?;
 
         if self.octave != 4 {
             write!(f, " @{}>", self.octave)
@@ -104,13 +104,10 @@ pub fn accidental(input: &str) -> IResult<&str, Accidental> {
 pub fn octave_count(input: &str) -> IResult<&str, i32> {
     let up_va = tag("'");
     let down_va = tag(",");
-    let up_run = many1_count(up_va);
-    let up_count = map(up_run, |n| n as i32);
-    let down_run = many1_count(down_va);
-    let down_count = map(down_run, |n| -1 * n as i32);
-    let up_or_down = alt((up_count, down_count));
+    let up_count = map(many1_count(up_va), |n| n as i32);
+    let down_count = map(many1_count(down_va), |n| -1 * n as i32);
     map(
-        fold_many0(up_or_down, Vec::new, |mut acc, item| {
+        fold_many0(alt((up_count, down_count)), Vec::new, |mut acc, item| {
             acc.push(item);
             acc
         }),
@@ -119,19 +116,26 @@ pub fn octave_count(input: &str) -> IResult<&str, i32> {
 }
 
 pub fn pitch(input: &str) -> IResult<&str, Note> {
-    let high = satisfy(|c| c >= 'A' && c <= 'G');
-    let low = satisfy(|c| c >= 'a' && c <= 'g');
+    let mut octave = 4;
+
+    let high = satisfy(|ch| ch >= 'A' && ch <= 'G');
+    let low = satisfy(|ch| ch >= 'a' && ch <= 'g');
+
+    let low = map(low, |ch| {
+        octave = octave - 1;
+        ch.to_ascii_uppercase()
+    });
 
     let some_key = alt((high, low));
 
     let note = tuple((opt(accidental), some_key, octave_count))(input)?;
 
-    let (rest, (acc, key, octave)) = note;
+    let (rest, (acc, key, mod_octave)) = note;
 
     Ok((
         rest,
         Note {
-            octave: octave + 4,
+            octave: octave + mod_octave,
             key: key,
             accidental: acc,
             length: 1,
@@ -170,11 +174,11 @@ fn parse_pitch() {
     );
     assert_eq!(
         // octave mark here is: 2 down + 2 up + 1 down
-        pitch("=B,,'',"),
+        pitch("=b,,'',"),
         Ok((
             "",
             Note {
-                octave: 3,
+                octave: 2,
                 key: 'B',
                 accidental: Some(Accidental::Natural),
                 length: 1
